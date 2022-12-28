f3bcbf4962bda58689e37f97712c8c921e6d1293
Perezdh13 <Perezdh13@gmail.com>
2022-12-28 10:34:16 +0100

    first clean code

--- __Test__/SheldonGame/sheldonGame.js
+++ __Test__/SheldonGame/sheldonGame.js
@@ -1,10 +1,50 @@
-function suma(a,b){
-  if (a > b){
-  return "gana tijeras"
+//var opciones = [piedra,papel,tijeras,lagarto,spock]
+var opciones =[0,1,2,3,4];
+
+
+function compara(a,b){
+ //usuario a escoge piedra
+  if (a == 0){
+    if(b == 1){ //usuario b escoge papel
+      return "gana papel"; 
+    }else{
+      if(b == 2){//usuario b escoge tijeras
+        return "gana piedra";
+      }else{
+        if(b == 3){//usuario b escoge lagarto
+          return "gana lagarto";
+        }else{
+          if(b == 4){//usuario b escoge spock
+            return "gana spock";
+          }
+        }
+      }
+
+    }
+    
   }
-  else if (a < b){
-    return "gana papel"
+
+//usuario a escoge papel
+  if (a == 1){
+  if(b == 0){ //usuario b escoge piedra
+    return "gana papel"; 
+  }else{
+    if(b == 2){//usuario b escoge tijeras
+      return "gana piedra";
+    }else{
+      if(b == 3){//usuario b escoge lagarto
+        return "gana lagarto";
+      }else{
+        if(b == 4){//usuario b escoge spock
+          return "gana spock";
+        }
+      }
+    }
+
   }
   
+  }
 }
- module.exports = suma;
+
+
+ module.exports = compara;
--- __Test__/sheldonGame.test.js
+++ __Test__/sheldonGame.test.js
@@ -1,6 +1,29 @@
 
-const suma = require("./SheldonGame/sheldonGame.js")
+const compara = require("./SheldonGame/sheldonGame.js")
+//si jugador a escoge piedra
+test("jugador 1 escoge piedra y jugador 2 escoge papel",()=>{
+  expect (compara(0,1)).toBe("gana papel");
+});
+test("jugador 1 escoge piedra y jugador 2 escoge tijeras",()=>{
+  expect (compara(0,2)).toBe("gana piedra");
+});
+test("jugador 1 escoge piedra y jugador 2 escoge lagarto",()=>{
+  expect (compara(0,3)).toBe("gana lagarto");
+});
+test("jugador 1 escoge piedra y jugador 2 escoge spock",()=>{
+  expect (compara(0,4)).toBe("gana spock");
+});
 
-test("si tijeras contra papel",()=>{
-  expect (suma(2,1)).toBe("gana tijeras");
+// si jugador b escoge papel
+test("jugador 1 escoge piedra y jugador 2 escoge papel",()=>{
+  expect (compara(1,0)).toBe("gana papel");
+});
+test("jugador 1 escoge piedra y jugador 2 escoge tijeras",()=>{
+  expect (compara(1,2)).toBe("gana piedra");
+});
+test("jugador 1 escoge piedra y jugador 2 escoge lagarto",()=>{
+  expect (compara(1,3)).toBe("gana lagarto");
+});
+test("jugador 1 escoge piedra y jugador 2 escoge spock",()=>{
+  expect (compara(1,4)).toBe("gana spock");
 });
